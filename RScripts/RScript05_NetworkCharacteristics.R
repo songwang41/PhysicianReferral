05109a75426e6a1ec8a60aa70f398f86547e7cdd
SongMadison <songwang@stat.wisc.edu>
2015-11-09 00:04:09 -0600

    calculate network characteristics

--- RScripts/RScript05_NetworkCharacteristics.R
+++ RScripts/RScript05_NetworkCharacteristics.R
@@ -1,7 +1,6 @@
 ### 
 
 #network characteristics:
-install.packages("igraph")
 library(data.table)  
 library(igraph)  
 setwd("~/Stat/Physician_Referral_Network")
@@ -12,14 +11,60 @@ PlotsPath <- "./Plots/"
 
 
 
-load(paste0(DataPath,"Network_Hsa0.RData"))
+load(paste0(DataPath,"Network_Hsa.RData"))
 
-# mean degree 
 
-# mean centrality ( pair-distance )
+Net_Hsa_features <- list()
+Net_Hsa_features$HSA = names(Networks_Hsa)
+# Net_Hsa_features$vcount <- numeric(length(Networks_Hsa)) 
+# Net_Hsa_features$degMean <- numeric(length(Networks_Hsa)) 
+# Net_Hsa_features$degSD <- numeric(length(Networks_Hsa)) 
+# Net_Hsa_features$edgeDensity <- numeric(length(Networks_Hsa))
+# Net_Hsa_features$transivity <- numeric(length(Networks_Hsa))
+# Net_Hsa_features$sizeOfcomponent <- numeric(length(Networks_Hsa))
+# Net_Hsa_features$clusteringCoef <- numeric(length(Networks_Hsa))
+# Net_Hsa_features$nComp <- numeric(length(Networks_Hsa))
+Net_Hsa_features <- data.frame(Net_Hsa_features)
 
-# mean clustering coefficients
+row.names(Net_Hsa_features) <- names(Networks_Hsa)
 
-# mean transitivity
+for(hsa in names(Networks_Hsa) ){
+  Edgelist <- Networks_Hsa[[hsa]]
+  g <- Edgelist[,.(V1,V2,V3)]
+  g <- graph.data.frame(g,directed = "FALSE")
+  E(g)$weight <- log(as.numeric(Edgelist$V3))
+  g <- simplify(g)
+  vc  <- vcount(g)
+  ec <- ecount(g)
+  clust <-  clusters(g)
+  ncomp <- clust$no
+  idx_max_comp <- which(clust$csize == max(clust$csize))
+  Net_Hsa_features[hsa,'sizeOfcomponent'] <- max(clust$csize[1])/vc
+  ## this features are based on the biggest connected component
+  
+  g2 <- induced.subgraph(g, which(clust$membership==idx_max_comp))
+  Net_Hsa_features[hsa,'edgeDensity'] <- 2* ec/vc/(vc-1)
+  Net_Hsa_features[hsa,'degMean'] <- mean( degree(g) )
+  Net_Hsa_features[hsa,'degSD'] <- sd( degree(g) )
+  Net_Hsa_features[hsa,'vcount'] <- vc
+  Net_Hsa_features[hsa,'ecount'] <- ec 
+  Net_Hsa_features[hsa,'closeness'] <- mean(closeness(g))
+  Net_Hsa_features[hsa,'edgeBetweenness'] <- mean(edge.betweenness(g))
+  Net_Hsa_features[hsa,'transivity'] <- transitivity(g)
+  Net_Hsa_features[hsa,'localClustCoef'] <- mean(transitivity(g,"local"),na.rm = T)
+  
+}
 
-# mean 
+write.csv(Net_Hsa_features,file= paste0(DataPath,"Net_Hsa_Features.csv"), row.names = F)
+clustCoef <- function(g){
+  if (vcount(g)< 5000){
+    
+  }
+}
+
+
+
+
+ 
+
+
